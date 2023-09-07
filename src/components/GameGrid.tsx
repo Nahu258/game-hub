63a225cf89e04953fbd782f5a20620e98af97903
Nahuel <Nahugc258@gmail.com>
2023-09-07 08:23:48 -0300

    Modify grid breakpoints

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,11 +11,19 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm:1, md:2, lg:3, xl:5 }} padding='10px' spacing={10}>
-        {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>)}
-        {data.map(response => <GameCardContainer key={response.id}>
-          <GameCard game={response}/>
-        </GameCardContainer>)}
+      <SimpleGrid columns={{ sm:2, md:3, xl:5 }} padding='10px' spacing={10}>
+        {isLoading && 
+          skeletons.map(skeleton => 
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton/>
+            </GameCardContainer>
+          )
+        }
+        {data.map(game => 
+          <GameCardContainer key={game.id}>
+            <GameCard game={game}/>
+          </GameCardContainer>
+        )}
       </SimpleGrid>
     </>
   )
